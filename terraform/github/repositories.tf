6f22d3e69470f1b97725ff6a6e7311983e21af73
Bernd Ruecker <bernd.ruecker@camunda.com>
2022-01-21 10:15:15 +0100

    chore(terraform): add new repository

--- terraform/github/repositories.tf
+++ terraform/github/repositories.tf
@@ -42,7 +42,8 @@ locals {
       "bpmn-driven-testing",
       "eze",
       "camunda-platform-to-cloud-migration",
-      "spring-zeebe"
+      "spring-zeebe",
+      "camunda-engine-rest-client-java"
     ],
   }
 }
